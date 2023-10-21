b5fcc3f068446c8f2b346d9727bd3370a5f0da52
Kostandin-Dervishaj <kostandin.dervishaj@gmail.com>
2023-10-21 22:44:06 +0200

    Minor addition to the rooms filter

--- src/components/RoomsFilter.tsx
+++ src/components/RoomsFilter.tsx
@@ -1,6 +1,9 @@
 import generateFloors from '@/helper/generateFloors';
 import { Radio } from 'antd';
-import React, { SetStateAction } from 'react';
+import React, {
+  useState,
+  SetStateAction,
+} from 'react';
 
 import styles from '../styles/cardRoom.module.css';
 
@@ -10,13 +13,15 @@ interface Props {
 }
 
 const RoomsFilter: React.FC<Props> = ({allRooms, setRooms}) => {
+  const [floorFilter,setFloorsFilter] = useState<string>('all');
   const floors = generateFloors();  
 
   return (
     <div className={styles.rooms_filter}>
       <h3 className={styles.rooms_filter_label}>Floors: </h3>
       <Radio.Group buttonStyle="solid" defaultValue='all' onChange={(e) => {
-          const value = e.target.value
+          const value = e.target.value;
+          setFloorsFilter(value);
           if(value === 'all'){
             setRooms(allRooms || [])
           } else {
@@ -29,6 +34,18 @@ const RoomsFilter: React.FC<Props> = ({allRooms, setRooms}) => {
             <Radio.Button key={floor.id} value={floor.id}>{floor.floorNumber}</Radio.Button>
             ))
           }
+      </Radio.Group>
+      <h3 className={styles.rooms_filter_label}>Availability: </h3>
+      <Radio.Group buttonStyle="solid" onChange={(e) => {
+        const value = e.target.value;
+        if(floorFilter === 'all'){
+          setRooms((allRooms || []).filter((room) => room.isAvailable === value));
+        } else {
+          setRooms((allRooms || []).filter((room) => room.isAvailable === value && room.name.charAt(1) === floorFilter))
+        }
+      }}>
+        <Radio.Button value={false}>Reserved</Radio.Button>
+        <Radio.Button value={true}>Available</Radio.Button>
       </Radio.Group>
     </div>
   )
